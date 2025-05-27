c62252f67ebfff9e513d71dfc7fd6a3841ad7d61
polisarath <sarathlalshaji@depaul.edu.in>
2025-05-27 22:13:19 +0530

    all changes added

---
+++ server/helpers/cloudinary.js
@@ -0,0 +1,37 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+require("dotenv").config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+const storage = new multer.memoryStorage();
+
+// async function imageUploadUtil(file) {
+//   const result = await cloudinary.uploader.upload(file, {
+//     resource_type: "auto",
+//   });
+
+//   return result;
+// }
+async function imageUploadUtil(buffer) {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { resource_type: "auto" },
+      (error, result) => {
+        if (error) return reject(error);
+        return resolve(result);
+      }
+    );
+
+    stream.end(buffer);
+  });
+}
+
+
+
+const upload = multer({ storage });
+
+module.exports = { upload, imageUploadUtil };
